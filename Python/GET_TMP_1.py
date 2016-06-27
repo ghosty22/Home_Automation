c44229275c2b4ad98979de1e518d839511c4f5a4
ghosty22 <ghostboy2@freenet.de>
2016-06-27 08:19:59 +0200

    Changed GET so Sensor Value is retrieved as Byte

--- Python/GET_TMP_1.py
+++ Python/GET_TMP_1.py
@@ -7,8 +7,13 @@ DEVICE_ADDRESS = 0x04      #7 bit address (will be left shifted to add the read 
 MESSAGE_BEGIN = 0x16
 MESSAGE_END = 0x06
 
+#Write an array of registers
+request_values = [0x53, MESSAGE_END]
+bus.write_i2c_block_data(DEVICE_ADDRESS, MESSAGE_BEGIN, request_values)
+
+
 #Read an array of registers
-data = bus.read_i2c_block_data(DEVICE_ADDRESS, 3)
+return_data = bus.read_i2c_block_data(DEVICE_ADDRESS, 3)
 
 import urllib
 
